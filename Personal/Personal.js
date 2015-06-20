f81b03a5750c0e8916cb23058379cead05929c37
Adam Wechter <wechtera@gmail.com>
2015-06-20 13:06:10 -0400

    First init

---
+++ Personal/Personal.js
@@ -0,0 +1,30 @@
+Router.map(function() {
+	this.route('home', {path: '/'});
+});
+
+if (Meteor.isClient) {
+	Template.zoombar.events({
+			//scrolling events
+		'click #zoomSkills': function(e, templ ) {
+			e.preventDefault();
+			$('html, body').animate({ 
+					scrollTop: $("#Skills").offset().top
+			}, 800);
+		},
+		'click #zoomProjects': function(e, templ ) {
+			e.preventDefault();
+			$('html, body').animate({ 
+					scrollTop: $("#Projects").offset().top
+			}, 800);
+		},
+		'click #zoomEducation': function(e, templ ) {
+			e.preventDefault();
+			$('html, body').animate({ 
+					scrollTop: $("#Education").offset().top
+			}, 800);
+		}
+	});
+	Template.home.rendered = function() {
+	};
+}
+
